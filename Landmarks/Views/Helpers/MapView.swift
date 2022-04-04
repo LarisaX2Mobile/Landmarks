3eb08aba55a3afb1960ceda4fe3828bee5ea6d55
Larisa Mihut <larisa.mihut@x2mobile.net>
2022-04-04 11:53:26 +0300

    Creating a macOS App

--- Landmarks/Views/Helpers/MapView.swift
+++ Landmarks/Views/Helpers/MapView.swift
@@ -10,10 +10,20 @@ import MapKit
 
 struct MapView: View {
     var coordinate: CLLocationCoordinate2D
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
+    @State private var region = MKCoordinateRegion()
+    
+    @AppStorage("MapView.zoom")
+    private var zoom: Zoom = .medium
+    
+    enum Zoom: String, CaseIterable, Identifiable {
+        case near = "Near"
+        case medium = "Medium"
+        case far = "Far"
+        
+        var id: Zoom {
+            return self
+        }
+    }
     
     var body: some View {
         Map(coordinateRegion: $region)
@@ -29,6 +39,7 @@ struct MapView: View {
         )
     }
 }
+
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Larisa Diana Mihuț on 04.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
